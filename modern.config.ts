5f8774d6b81f320db82a3c908655ba8feccd290b
KotoriK <52659125+KotoriK@users.noreply.github.com>
2023-02-23 15:51:49 +0800

    ini

--- modern.config.ts
+++ modern.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig } from '@modern-js/app-tools';
+import appTool,{ defineConfig } from '@modern-js/app-tools';
 
 // https://modernjs.dev/docs/apis/app/config
 export default defineConfig({
   runtime: {
-    router: true,
-    state: true,
   },
+  plugins:[appTool()],dev:{port:3000}
 });
